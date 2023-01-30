8f676470f688fe3122ee84b0d95604282232d769
RodriMR <rodrimr2008@gmail.com>
2023-01-30 18:54:48 -0300

    First commit chat gpt with react

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <aside className="sidemenu">
+        <h1>Aside</h1>
+      </aside>
+    </div>
+  );
+}
+
+export default App;
