741ca78c1928f2747d80652f8d363b0979203d9a
RodriMR <rodrimr2008@gmail.com>
2023-01-31 20:23:46 -0300

    visual structure separation

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
-import logo from "./logo.svg";
 import "./App.css";
+import "./normal.css";
 
 function App() {
   return (
     <div className="App">
       <aside className="sidemenu">
-        <h1>Aside</h1>
+        <div className="side-menu-button">
+          <span>+</span> New Chat
+        </div>
       </aside>
+      <section className="chatbox">
+        <div className="chat-input-holder">
+          <textarea
+            className="chat-input-textarea"
+            placeholder="Type your message here"
+          ></textarea>
+        </div>
+      </section>
     </div>
   );
 }
