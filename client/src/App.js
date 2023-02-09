97ce64c9b9371149f7ebdce6a753fc2746d9294f
RodriMR <rodrimr2008@gmail.com>
2023-02-09 13:08:56 -0300

    chat working

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,35 @@
 import "./App.css";
 import "./normal.css";
-
+import { useState } from "react";
 function App() {
+  const [input, setInput] = useState("");
+  const [chatLog, setChatLog] = useState([
+    {
+      user: "gpt",
+      message: "How can I help you today?",
+    },
+    {
+      user: "me",
+      message: "I wanted to use ChatGpt today",
+    },
+  ]);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setInput("");
+    setChatLog([...chatLog, { user: "me", message: input }]);
+    const response = await fetch("http://localhost:3080", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        message: chatLog.map((message) => message.message).join(""),
+      }),
+    });
+
+    const data = await response.json();
+    setChatLog([...chatLog, { user: "gpt", message: data.message }]);
+  };
   return (
     <div className="App">
       <aside className="sidemenu">
@@ -11,28 +39,39 @@ function App() {
       </aside>
       <section className="chatbox">
         <div className="chat-log">
-          <div className="chat-message ">
-            <div className="chat-message-center">
-              {" "}
-              <div className="avatar"></div>
-              <div className="message">Hello world</div>
-            </div>
-          </div>
-        </div>
-        <div className="chat-log">
-          <div className="chat-message chatgpt">
-            <div className="chat-message-center">
-              <div className="avatar chatgpt"></div>
-              <div className="message">I am an ai</div>
-            </div>
-          </div>
+          {chatLog.map((message, index) => {
+            console.log(chatLog);
+            return <ChatMessage message={message} />;
+          })}
         </div>
         <div className="chat-input-holder">
-          <textarea rows="1" className="chat-input-textarea"></textarea>
+          <form onSubmit={handleSubmit}>
+            <input
+              value={input}
+              onChange={(e) => {
+                setInput(e.target.value);
+              }}
+              rows="1"
+              className="chat-input-textarea"
+            />
+          </form>
         </div>
       </section>
     </div>
   );
 }
 
+const ChatMessage = ({ message, index }) => {
+  return (
+    <div
+      key={index}
+      className={`chat-message ${message.user === "gpt" && "chatgpt"}`}
+    >
+      <div className="chat-message-center">
+        <div className={`avatar ${message.user === "gpt" && "chatgpt"}`}></div>
+        <div className="message">{message.message}</div>
+      </div>
+    </div>
+  );
+};
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,9 +1,31 @@
-//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
+const { Configuration, OpenAIApi } = require("openai");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-import { Configuration, OpenAIApi } from "openai";
 const configuration = new Configuration({
   organization: "org-3U9ymM8BB8EEx0zERLnGWtg4",
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: "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",
 });
 const openai = new OpenAIApi(configuration);
-const response = await openai.listEngines();
+// const response = await openai.listEngines();
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+const port = 3080;
+app.post("/", async (req, res) => {
+  const { message } = req.body;
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: message,
+    max_tokens: 100,
+    temperature: 0.5,
+  });
+
+  res.json({ message: response.data.choices[0].text });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
